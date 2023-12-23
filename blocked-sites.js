eb4241d2c4eb342041bdbd06fe40d1b6d3082398
milosz-student <milosz.ilecki@gmail.com>
2023-12-23 20:05:04 +0100

    Added button for deleting blocked page from the list

--- blocked-sites.js
+++ blocked-sites.js
@@ -1,10 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', setupBlockedSitesList);
+
+function setupBlockedSitesList() {
     chrome.storage.local.get('blockedSites', ({ blockedSites }) => {
         const blockedSitesList = document.getElementById('blockedSitesList');
+        blockedSitesList.innerHTML = '';
+
         if (blockedSites && blockedSites.length > 0) {
             blockedSites.forEach(site => {
-                const listItem = document.createElement('li');
-                listItem.textContent = site.url;
+                const listItem = createBlockedSiteListItem(site);
                 blockedSitesList.appendChild(listItem);
             });
         } else {
@@ -13,4 +16,28 @@ document.addEventListener('DOMContentLoaded', () => {
             blockedSitesList.appendChild(noBlockedSitesItem);
         }
     });
-});
+}
+
+function createBlockedSiteListItem(site) {
+    const listItem = document.createElement('li');
+    listItem.textContent = site.url;
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', () => {
+        removeBlockedSite(site);
+        listItem.remove();
+    });
+
+    listItem.appendChild(removeButton);
+    return listItem;
+}
+
+function removeBlockedSite(site) {
+    chrome.storage.local.get('blockedSites', ({ blockedSites }) => {
+        const updatedBlockedSites = blockedSites.filter(item => item.url !== site.url);
+        chrome.storage.local.set({ blockedSites: updatedBlockedSites }, () => {
+            console.log('Site removed:', site);
+        });
+    });
+}
