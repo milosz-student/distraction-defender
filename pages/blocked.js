19736e8296aa2ddfed2335cd3c142200a067f84b
milosz-student <milosz.ilecki@gmail.com>
2024-01-02 15:58:49 +0100

    Add dynamic background image to blocked page

--- pages/blocked.js
+++ pages/blocked.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     try {
+        chrome.storage.local.get('backgrounds', function (data) {
+            const backgroundFolder = '../images/background/';
+            const backgroundImages = data.backgrounds || [];
+        
+            if (backgroundImages.length > 0) {
+                const randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+                document.body.style.backgroundImage = `url('${backgroundFolder}${randomImage}')`;
+            } else {
+                console.error('No background images found.');
+            }
+        });
+        
         const urlParams = new URLSearchParams(window.location.search);
         const blockedSite = urlParams.get('site');
-        console.log('blockedSite:', blockedSite);
+        
         if (blockedSite) {
             const blockedSiteUrlElement = document.getElementById('blockedSiteUrl');
+            
             if (blockedSiteUrlElement) {
                 blockedSiteUrlElement.textContent = decodeURIComponent(blockedSite);
             } else {
