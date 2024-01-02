0668ffe99d430743f24e71fd5c32ff8748b3dc8b
milosz-student <milosz.ilecki@gmail.com>
2024-01-02 15:59:07 +0100

    Add default backgrounds and set initial backgrounds in local storage

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+const defaultBackgrounds = ['background-1.png', 'background-2.png', 'background-3.png', 'background-4.png', 'background-5.png', 'background-6.png'];
+
+chrome.storage.local.get({ backgrounds: defaultBackgrounds }, function (data) {});
+
+chrome.storage.local.set({ backgrounds: defaultBackgrounds }, function () {
+    if (chrome.runtime.lastError) {
+        console.error('Error:', chrome.runtime.lastError);
+    } else {
+        console.log('Initial backgrounds set successfully.');
+    }
+});
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.redirect) {
